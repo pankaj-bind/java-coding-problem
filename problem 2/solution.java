fe626cd88d2c9144e6d31e1fcc82d23de51fc423
Pankaj Kumar Bind <73558583+Pankaj-Bind@users.noreply.github.com>
2023-05-25 00:36:12 +0530

    Add files via upload

---
+++ problem 2/solution.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+public class solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+        if(N%2==1){
+            System.out.println("Weird");
+        }
+        else if(N%2==0 && N<=5 && N>=2){
+            System.out.println("Not Weird");
+        }
+        else if(N%2==0 && N<=20 && N>=6){
+            System.out.println("Weird");
+        }
+        else if(N%2==0 && N>20){
+            System.out.println("Not Weird");
+        }
+    }
+}
+
